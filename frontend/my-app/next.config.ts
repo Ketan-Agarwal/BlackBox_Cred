8b83e7129d29d670cebaaa85aa37fdc23b4ed9c8
Ketan <ketan.agarwal2506@gmail.com>
2025-08-22 20:13:31 +0530

    fix: DOckerfile

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -4,14 +4,6 @@ const BACKEND = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${BACKEND}/:path*`
-      }
-    ]
-  }
 };
 
 export default nextConfig;
